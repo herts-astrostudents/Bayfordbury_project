223fa4c5df37e7cd597fd6d34fc3ff20b34fb5dc
Max Timberhill <max.lisogorski@gmail.com>
2020-03-02 18:12:30 +0000

    added a credentials file

--- scripts/bay.py
+++ scripts/bay.py
@@ -1,3 +1,4 @@
 from bayfordbury.api import ArchiveApi
+from credentials import api_key, observer_id
 
-api = ArchiveApi(api_key="", observer_id=0)
+api = ArchiveApi(api_key=api_key, observer_id=observer_id)
